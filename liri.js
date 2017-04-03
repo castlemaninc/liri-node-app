fed856ee1a1e8c967250a0ae9366d34a5fd1fb3b
Bryan Castleman <Bryan Castleman>
2017-04-02 17:39:11 -0700

    adding initial js files and .txt files for project

---
+++ liri.js
@@ -0,0 +1,9 @@
+// Make it so liri.js can take in one of the following commands:
+	// my-tweets 
+	// spotify-this-song 
+	// movie-this 
+	// do-what-it-says 
+
+var request = require('request');
+var spotify = require('spotify'); 
+var twitter = require('twitter');
