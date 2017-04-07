61a7f89a363d07dc8c6e5f53b0d72afdba5b1d82
Bryan Castleman <Bryan Castleman>
2017-04-07 00:39:22 -0700

    adding updates to Spotify and Twitter functionality

--- liri.js
+++ liri.js
@@ -12,17 +12,10 @@ var client = new Twitter(keys.twitterKeys);
 	// console.log(keys.twitterKeys);
 	// console.log(keys.twitterKeys.consumer_key);
 
-// 5. Make it so liri.js can take in one of the following commands:
-	// my-tweets 
-	// spotify-this-song 
-	// movie-this 
-	// do-what-it-says 
-
 
 // TWITTER 
 
-// This will show your last 20 tweets and when they were created at in your terminal/bash window.
-
+// This will show my last 20 tweets and when they were created in terminal/bash
 if (process.argv[2] === "my-tweets"){	
 
 	var params = {screen_name: "wutup_castleman"};	
@@ -32,14 +25,18 @@ if (process.argv[2] === "my-tweets"){
 		  	console.log('error:', error); // Print the error if one occurred 
 		  	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
 	  	} else {
-	  		console.log("Bryan tweeted: '" + tweets["statuses"][0].text + "' Tweeted on: " + tweets["statuses"][0]["created_at"]);
+	  		for (i=0;i<tweets["statuses"].length;i++){
+	  			console.log("----------------------");
+	  			console.log("Bryan tweeted: '" + tweets["statuses"][i].text +"'"); 
+	  			console.log("Tweeted on: " + tweets["statuses"][i]["created_at"]);
+	  		}
+	  		
 	  	}
 	});
 		
 };
 
-// SPOTIFY 
-
+// SPOTIFY
 if (process.argv[2] === 'spotify-this-song'){
 	
 	//  node liri.js spotify-this-song '<song name here>' 
@@ -50,7 +47,11 @@ if (process.argv[2] === 'spotify-this-song'){
   		song = song + " " + nodeArgs[i];
 	}
 	
-	console.log("Searching for:" + song);
+	if (nodeArgs.length === 3){
+		song = "Midnight City";
+	}
+
+	console.log("Searching for: " + song);
 	
 	Spotify.search({ type: 'track', query: song }, function(error, data) {
 	    
@@ -58,27 +59,25 @@ if (process.argv[2] === 'spotify-this-song'){
 	        console.log('Error occurred: ' + error);
 	        return;
 	    } else {
-	    	console.log(data);	    	
-
-	    	// ◦This will show the following information about the song in your terminal/bash window
-			// ◾Artist(s)
-			// ◾The song's name
-			// ◾A preview link of the song from Spotify
-			// ◾The album that the song is from
+	    	console.log("---------------------");
+	    	// Artist(s)
+	    	console.log("Artist(s): " + data["tracks"].items[1].artists[0].name);
+	    	// The song's name
+	    	console.log("Song Title: " + data["tracks"].items[0].name);
+	    	// A preview link of the song from Spotify
+	    	console.log("Preview Link: " + data["tracks"].items[0].preview_url);
+	    	// The album that the song is from
+	    	console.log("Album Name: " + data["tracks"].items[0].album.name);
+	    	console.log("---------------------");			
 	    }
  
 	});
-	
-
 	// ◦if no song is provided then your program will default to
 	// ◾"The Sign" by Ace of Base
-
-	
 
 };
 
 // OMDB API using REQUEST
-
 if (process.argv[2] === 'movie-this'){
 	console.log("Movie this?");
 
